3ccc8a5865b4feb666ac9d0c86f4a1924e14e547
Remy Bach <remy@bach.me.uk>
2015-07-01 15:05:32 +0100

    Massive Visual Overhaul

--- index.js
+++ index.js
@@ -1,36 +1,98 @@
 var API_KEY = "19e94d3006c34e34a3087ee92a1d9f67",
-
+    URLS = {
+      activity: "http://www.bungie.net/platform/Destiny/Stats/PostGameCarnageReport/",
+      definitions: "https://www.bungie.net/Platform/Destiny/Stats/Definition/",
+      manifest: "https://www.bungie.net/Platform/Destiny/Manifest/"
+    },
+    JSON_REG = /\.json$/i,
+  
+    bodyParser = require('body-parser'),
     express = require('express'),
     request = require("request"),
-    app = express();
+    Q = require('q'),
+    Data = require('./utils/data.js').Data,
+    app = express(),
+    definitions;
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());     // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+  extended: true
+}));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index')
+app.post('/', function(req, res) {
+  if (req.body.activityId) {
+    res.redirect('/' + req.body.activityId);
+  } else {
+    res.render('pages/index', { error: false });
+  }
 });
+app.get('/', function(req, res) {
+  res.render('pages/index', { error: false });
+});
+app.get('/:activityId', function(req, res) {
+  if (/[0-9]+(\.json)?/.test(req.params.activityId)) {
+    if (JSON_REG.test(req.params.activityId)) {
+      getActivityData(req.params.activityId.replace(JSON_REG, ''), function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+          res.send(body);
+        }
+      });
+    } else {
+      getActivityData(req.params.activityId, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+          var data = JSON.parse(body),
+              dataUtil;
 
-app.get('/:activity', function (req, res) {
-  request({
-    url: "http://www.bungie.net/platform/Destiny/Stats/PostGameCarnageReport/" + req.params.activity,
-    headers: {
-      "X-API-Key": API_KEY
+          if (data) {
+            dataUtil = new Data(data.Response.data, definitions, API_KEY, URLS);
+
+            Q.fcall(dataUtil.tidyUp.bind(dataUtil)).then(function(tidiedData) {
+              res.render('pages/activity', { error: false, data: tidiedData, activityId: req.params.activityId });
+            }).fail(function(error) {
+              res.render('pages/activity', { error: error });
+            });
+
+          } else {
+            res.render('pages/index', { error: "No data was returned for this activity id." });
+          }
+        }
+      });  
     }
-  }, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      res.send(body);
-    }
-  });
+  } else {
+    res.render('pages/index', { error: false });
+  }
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+function getActivityData(activityId, callback) {
+  console.log('Looking up data for activity with id: ' + activityId);
+  request({
+    url: URLS.activity + activityId,
+    headers: {
+      "X-API-Key": API_KEY
+    }
+  }, callback);
+}
 
+request({
+  url: URLS.definitions,
+  headers: {
+    "X-API-Key": API_KEY
+  }
+}, function(error, response, body) {
+  if (!error && response.statusCode == 200) {
+    definitions = JSON.parse(body).Response;
+  } else {
+    throw Error(error);
+  }
+});
---
+++ utils/data.js
@@ -0,0 +1,202 @@
+var Data = function(data, definitions, API_KEY, URLS) {
+  this.data = data;
+  this.definitions = definitions;
+  this.API_KEY = API_KEY;
+  this.URLS = URLS;
+},
+
+    request = require("request"),
+    Q = require("q");
+
+Data.prototype.tidyUp = function() {
+  tidy = {
+    activity: {
+      period: this.data.period.replace(/[TZ]/g, ' ')
+    },
+    gameInfoEncountered: []
+  };
+
+  tidy.players = {};
+
+  for (var i=0; i < this.data.entries.length; i++) {
+    var entry = this.data.entries[i];
+
+    tidy.players[entry.player.destinyUserInfo.displayName] = this.parsePlayer(entry);
+  }
+
+  // These calls are async, so return a promise.
+  return Q.fcall(function() {
+    var callPromise,
+        calls = [],
+        toCall,
+        dfd = Q.defer();
+
+    // Activity Name and Level
+    toCall = function(error, response, body) {
+      if (!error && body) {
+        var activity = JSON.parse(body).Response.data.activity;
+
+        tidy.activity.name = activity.activityName;
+        tidy.activity.level = activity.activityLevel;
+        tidy.activity.icon = "http://bungie.net" + activity.icon;
+
+        this.dfd.resolve();
+      } else {
+        this.dfd.reject(new Error(error));
+      }
+    }.bind({ dfd: dfd })
+    this.getActivityInfo(this.data.activityDetails.referenceId, toCall);
+    calls.push(dfd.promise);
+
+    // Weapon Name and Description
+    for (player in tidy.players) {
+      var player = tidy.players[player],
+          enemiesEncountered = player.enemiesEncountered,
+          weaponInfo = player.weaponInfo;
+
+      for (var i=0; i < weaponInfo.length; i++) {
+        var weapon = weaponInfo[i];
+
+        dfd = Q.defer();
+
+        toCall = function(error, response, body) {
+          if (!error && body) {
+            var weapon = JSON.parse(body).Response.data.inventoryItem;
+
+            this.weapon.name = weapon.itemName;
+            this.weapon.description = weapon.itemDescription;
+            this.weapon.icon = "http://bungie.net" + weapon.icon;
+
+            this.dfd.resolve();
+          } else {
+            this.dfd.reject(new Error(error));
+          }
+        }.bind({ weapon: weaponInfo[i], dfd: dfd });
+
+        this.getWeaponInfo(weapon.id, toCall);
+        calls.push(dfd.promise);
+      }
+    }
+
+    return Q.all(calls);
+  }.bind(this)).then(function() {
+    console.log('returning the tidied up list.');
+    return tidy;
+  });
+};
+
+Data.prototype.parsePlayer = function(entry) {
+  var player = {
+        // Character info
+        characterClass: { name: 'Class', value: entry.player.characterClass },
+        characterLevel: { name: 'Level', value: entry.player.characterLevel },
+        clanName: { name: 'Clan Name', value: entry.player.clanName },
+        clanTag: { name: 'Clan Tag', value: entry.player.clanTag }
+      },
+      gameInfo = {},
+      enemyInfo = { assists: {}, deaths: {}, kills: {}, precision: {} },
+      weaponInfo = [],
+      weaponClassInfo = { kills: {}, precision: {} },
+      enemiesEncountered = [],
+      weaponsEncountered = [],
+      weaponClassInfoEncountered = [],
+
+      NOT = "?!",
+      ENEMIES = "assistsAgainst|deathsFrom|killsOf|precisionKillOf",
+      USELESS = "allParticipants|fireTeamId|weaponKillsPrecisionKills|uniqueWeaponKillsPrecisionKills",
+      WEAPONS = "weapon";
+
+  for (key in entry.extended.values) {
+    var val = entry.extended.values[key].basic.displayValue;
+
+    if (new RegExp("^(" + NOT + USELESS + "|" + ENEMIES + "|" + WEAPONS + ").*$").test(key)) {
+
+      // Regular old game info.
+      gameInfo[key] = val;
+
+      tidy.gameInfoEncountered.indexOf(key) == -1 && tidy.gameInfoEncountered.push(key);
+
+    } else if (new RegExp("^(" + NOT + USELESS + ")(" + WEAPONS + ").*$").test(key)) {
+
+      var name;
+
+      // Weapon info
+
+      if (key.indexOf('weaponKills') > -1) {
+        name = key.replace(/^weaponKills/, '');
+        weaponClassInfo['kills'][name] = val;
+      } else if (key.indexOf('weaponPrecisionKills') > -1) {
+        name = key.replace(/^weaponPrecisionKills/, '');
+        weaponClassInfo['precision'][name] = val;
+      }
+
+      name && weaponClassInfoEncountered.indexOf(name) == -1 && weaponClassInfoEncountered.push(name);
+
+    } else if (new RegExp("^(" + NOT + USELESS + ")(" + ENEMIES + ").*$").test(key)) {
+
+      var name = this.definitions[key].statName,
+          enemy, type = '';
+
+      // Enemy info
+
+      if (key.indexOf('assistsAgainst') > -1) {
+        key = key.replace(/assistsAgainst/, '');
+        type = 'assists';
+      } else if (key.indexOf('deathsFrom') > -1) {
+        key = key.replace(/deathsFrom/, '');
+        type = 'deaths';
+      } else if (key.indexOf('killsOf') > -1) {
+        key = key.replace(/killsOf/, '');
+        type = 'kills';
+      } else if (key.indexOf('precisionKillOf') > -1) {
+        key = key.replace(/precisionKillOf/, '');
+        type = 'precision';
+      }
+
+      enemy = { key: key, val: val, name: name };
+      enemyInfo[type][key] = enemy;
+      enemiesEncountered.indexOf(enemy) == -1 && enemiesEncountered.push(enemy);
+
+    }
+
+    for (key in entry.extended.weapons) {
+      var val = entry.extended.weapons[key];
+
+      if (weaponsEncountered.indexOf(val.referenceId) == -1) {
+        weaponInfo.push({
+          id: val.referenceId,
+          kills: val.values.uniqueWeaponKills.basic.displayValue,
+          precision: val.values.uniqueWeaponPrecisionKills.basic.displayValue
+        });
+        weaponsEncountered.push(val.referenceId);
+      }
+    }
+
+    player.gameInfo = gameInfo;
+    player.enemyInfo = enemyInfo;
+    player.enemiesEncountered = enemiesEncountered;
+    player.weaponInfo = weaponInfo;
+    player.weaponClassInfo = weaponClassInfo;
+    player.weaponsEncountered = weaponsEncountered;
+    player.weaponClassInfoEncountered = weaponClassInfoEncountered;
+  }
+
+  return player;
+};
+
+Data.prototype.getActivityInfo = function(id, callback) {
+  request({
+    url: this.URLS.manifest + '1/' + id, // 1 = activity
+  }, callback);
+};
+
+Data.prototype.getWeaponInfo = function(id, callback) {
+  request({
+    url: this.URLS.manifest + '6/' + id, // 6 = weapons
+    headers: {
+      "X-API-Key": this.API_KEY
+    }
+  }, callback);
+};
+
+exports.Data = Data;
