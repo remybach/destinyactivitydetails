22634b411f33ea498ec284622d7a30697f194b92
Remy Bach <remy@bach.me.uk>
2015-07-03 13:24:09 +0100

    Enable hashlinking to a particular player

--- public/scripts.js
+++ public/scripts.js
@@ -8,4 +8,19 @@ jQuery(function ($) {
     paging: false,
     searching: false
   });
+
+  // Enable hotlinking directly to players.
+  $('.js-player-name').on('click', function() {
+    $(this).parent().find('.js-collapse-toggle').trigger('click');
+  });
+  
+  if (location.hash) {
+    // This is absurd, but seemingly necessary to get the page to scroll to the hash on load.
+    $("#collapse-" + location.hash.slice(1)).collapse('show').one($.support.transition.end, function() {
+      var hash = location.hash;
+
+      location.hash = "";
+      location.hash = hash;
+    });
+  }
 });
